642f8d5a68c9147fde470ab3bea0d2599eeb662c
LuckRain <763839938@qq.com>
2019-11-07 10:30:58 +0800

    async_await

--- Demo/JavaScript/ES7ES8/async_await.js
+++ Demo/JavaScript/ES7ES8/async_await.js
@@ -1,1 +1,19 @@
+
 // ES8 内容
+async function getStockPriceByName(name) {
+    const symbol = await getStockSymbol(name);
+    const stockPrice = await getStockPrice(symbol);
+    return stockPrice;
+}
+
+getStockPriceByName('goog').then(function (result) {
+    console.log(result);
+});
+
+// async函数的语法规则总体上比较简单，难点是错误处理机制。
+
+
+
+// getFoo和getBar都是同时触发，这样就会缩短程序的执行时间。
+let [foo, bar] = await Promise.all([getFoo(), getBar()]);
+
